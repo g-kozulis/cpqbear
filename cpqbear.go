2d173636cc0dc053f8c01d0a4eab57758129cc19
Georgs Kozulis <g.kozulis@gmail.com>
2023-09-16 17:33:17 +0300

    Add JWT retrieval

--- cpqbear.go
+++ cpqbear.go
@@ -12,6 +12,10 @@ type AccessTokenResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
+type JWTResponse struct {
+	JWT string `json:"token"`
+}
+
 func GetAccessToken(url string, username string, password string) (string, error) {
 	payload := []byte("grant_type=password&username=" + username + "&password=" + password)
 
@@ -46,4 +50,41 @@ func GetAccessToken(url string, username string, password string) (string, error
 	}
 
 	return tokenResponse.AccessToken, nil
+}
+
+func GetJWT(url string, accessToken string) (string, error) {
+	authorization := "Bearer " + accessToken
+
+	request, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	request.Header.Set("Authorization", authorization)
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	var tokenResponse JWTResponse
+
+	err = json.Unmarshal(responseBody, &tokenResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenResponse.JWT, nil
 }
